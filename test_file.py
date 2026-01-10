8515aa3ac63f2b075248740e93c0da8158650b62
Michael Nadolsky <mwnadolsky@gmail.com>
2026-01-09 23:59:08 -0500

    add selenium python hello-world

---
+++ test_file.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+
+
+def test_google_title():
+
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+
+    driver.quit()
