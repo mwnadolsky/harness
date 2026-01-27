6e4ab88fd74e321073e26090ebe02c2c81a3ae1e
Michael Nadolsky <mwnadolsky@gmail.com>
2026-01-27 13:22:04 -0500

    push for tony

--- test_file.py
+++ test_file.py
@@ -1,12 +1,112 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By as by
+import pytest
 
 
 
-def test_google_title():
+
+def test_title():
 
     driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/")
 
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    assert "The Internet" in driver.title
+
+    driver.quit()
+
+
+def test_ab_testing():
+
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/")
+
+    link = driver.find_element('xpath', '//a[text()="A/B Testing"]')
+    link.click()
+
+    # get the first line of text on the page
+    page_text = driver.find_element(by.TAG_NAME, 'body').text
+    first_line = next(line for line in page_text.splitlines() if line.strip())
+
+    assert first_line == 'A/B Test Variation 1' or first_line == 'A/B Test Control'
+
+    driver.quit()
+
+
+def test_add_elements():
+
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/")
+
+    driver.find_element('xpath', '//a[text()="Add/Remove Elements"]').click()
+
+    buttons = driver.find_elements('xpath', '//button[@class="added-manually"]')
+
+    assert len(buttons) == 0
+
+    add_element_button = driver.find_element('xpath', '//button[text()="Add Element"]')
+    add_element_button.click()
+    buttons = driver.find_elements('xpath', '//button[@class="added-manually"]')
+
+    assert len(buttons) == 1
+
+    add_element_button.click()
+    buttons = driver.find_elements('xpath', '//button[@class="added-manually"]')
+
+    assert len(buttons) == 2
+
+    driver.quit()
+
+
+def test_remove_elements():
+
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/")
+
+    driver.find_element('xpath', '//a[text()="Add/Remove Elements"]').click()
+
+    driver.find_element('xpath', '//button[text()="Add Element"]').click()
+
+    buttons = driver.find_elements('xpath', '//button[@class="added-manually"]')
+
+    assert len(buttons) == 1
+
+    driver.find_element('xpath', '//button[text()="Delete"]').click()
+
+    buttons = driver.find_elements('xpath', '//button[@class="added-manually"]')
+
+    assert len(buttons) == 0
+
+    driver.quit()
+
+
+def test_basic_auth_login():
+
+    driver = webdriver.Chrome()
+    driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
+
+    # get the first line of text on the page
+    page_text = driver.find_element(by.TAG_NAME, 'body').text
+    first_line = next(line for line in page_text.splitlines() if line.strip())
+
+    assert first_line == 'Basic Auth'
+
+def test_checkboxes():
+
+    driver = webdriver.Chrome()
+    driver.get("https://admin:admin@the-internet.herokuapp.com")
+
+    driver.find_element('xpath', '//a[text()="Checkboxes"]').click()
+
+    box_1 = driver.find_element('xpath', '//input[@type="checkbox" and normalize-space(following-sibling::text()[1])="checkbox 1"]')
+    box_2 = driver.find_element('xpath', '//input[@type="checkbox" and normalize-space(following-sibling::text()[1])="checkbox 2"]')
+
+    assert not box_1.is_selected()
+    assert box_2.is_selected()
+
+    box_1.click()
+    box_2.click()
+
+    assert box_1.is_selected()
+    assert not box_2.is_selected()
 
     driver.quit()
