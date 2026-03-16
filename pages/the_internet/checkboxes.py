0ba7b2d4c10de4c498765042072c7b0177fb9c5d
Michael Nadolsky <mwnadolsky@gmail.com>
2026-03-15 23:21:05 -0400

    Revert "add checkboxes page object and a file for testing page objects"
    This reverts commit 0dadc061a72744986a2de74c58e52cafb8a640cb.

--- pages/the_internet/checkboxes.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class CheckboxesPage:
-
-    header = '//h3'
-    checkbox2 = '//input[following-sibling::text()[1][normalize-space()="checkbox 1"]]'
-    checkbox2 = '//input[following-sibling::text()[1][normalize-space()="checkbox 2"]]'
