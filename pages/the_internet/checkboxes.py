0dadc061a72744986a2de74c58e52cafb8a640cb
Michael Nadolsky <mwnadolsky@gmail.com>
2026-03-15 21:04:57 -0400

    add checkboxes page object and a file for testing page objects

---
+++ pages/the_internet/checkboxes.py
@@ -0,0 +1,8 @@
+
+
+
+class CheckboxesPage:
+
+    header = '//h3'
+    checkbox2 = '//input[following-sibling::text()[1][normalize-space()="checkbox 1"]]'
+    checkbox2 = '//input[following-sibling::text()[1][normalize-space()="checkbox 2"]]'
